47643992202e1d976baf5331d226994133b8a251
GAL_son <pancerek1@gmail.com>
2023-11-11 15:59:56 +0100

    File reading complited

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils import read_dimcas_file
 
 def main():
     # Test reading from file
-    path = "./data/test-file.txt"
+    path = "data/easy/sat/Analiza1-itox_vc1033.cnf"
     print(read_dimcas_file(path))
 
     return
