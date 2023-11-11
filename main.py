099f4a56782fed49001c637493a6e4b18e26200f
ldgeyexea <piotrek269962@gmail.com>
2023-11-11 17:19:02 +0100

    added comments, comented trash

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ def main():
     return
 
 main()
-import statement
-def analyseFile(filePath):
-    file=open(filePath)
-    file.readline()
-    statement_string = ""
-    for line in file:
-        statement_string += line
-    return statement.Statement(statement_string)
-
-
-
-
-test=analyseFile("asset/test1.cnf")
-
-
-for i in test.clauses:
-    print(i)
-
-print(test.variables_already_existing)
-for i in test.variables:
-    print(i)
+# to ponizej ja dodalem, jak widzisz to ianczej to wywal
+# import statement
+# def analyseFile(filePath):
+#     file=open(filePath)
+#     file.readline()
+#     statement_string = ""
+#     for line in file:
+#         statement_string += line
+#     return statement.Statement(statement_string)
+#
+#
+#
+#
+# test=analyseFile("asset/test1.cnf")
+#
+#
+# for i in test.clauses:
+#     print(i)
+#
+# print(test.variables_already_existing)
+# for i in test.variables:
+#     print(i)
