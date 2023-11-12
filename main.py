4066cd4b929f53c22f088941d443357376319f89
GAL_son <pancerek1@gmail.com>
2023-11-12 19:03:15 +0100

    I had enough - it draws something

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from methods.test.test1 import methodTest1
 def main():
     # Test reading from file
     path = "data/test-cnf.cnf"
-    path2 = "data/easy/sat/Analiza1-itox_vc1033.cnf"
-    file = read_dimcas_file(path)
+    path2 = "asset/test1.cnf"
+    path3 = "asset/testshort.cnf"
+    file = read_dimcas_file(path3)
     test = Statement(file)   
 
     methodTest1(test)
--- methods/test/test1.py
+++ methods/test/test1.py
@@ -10,22 +10,33 @@ def methodTest1(statement):
     
     # In this graph each vertex represents one clause
     vert_num = len(statement.clauses)
+    vert_strenght = []
+    edge_strenght = []
+    vert_name = []
+
 
     # Each vertex is connected if there is at least one common variable
     connections=[]
     for i in range(0,vert_num):
+        clause1 = statement.clauses[i]
+        vert_strenght.append(len(clause1.variables))
+        vert_name.append(clause1.name)
         for j in range(i+1, vert_num):
-            clause1 = statement.clauses[i]
             clause2 = statement.clauses[j]
             for v in clause1.variables:
-                if v in clause2.variables:
+                
+                if v in clause2.variables or -v in clause2.variables:
                     connections.append([i,j])
                     break
-    
-                    
+   
+    # Plot graph
     g = ig.Graph(n=vert_num, edges=connections)   
+    g.vs["label"] = vert_name
+    g.vs["size"]  = ig.rescale(vert_strenght, (10, 50))
+
+    print(g.edge_betweenness())
     fig, ax = plt.subplots()
-    ig.plot(g, target=ax)
+    ig.plot(g, target=ax, layout="auto")
 
     plt.show()
     
