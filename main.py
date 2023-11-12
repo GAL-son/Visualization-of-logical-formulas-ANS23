c7587530cddbf5b0a4a3caaef5c5fa14a952a79b
GAL_son <pancerek1@gmail.com>
2023-11-12 12:12:38 +0100

    Statement building + file reading test

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 from utils import read_dimcas_file
+from statement import Statement
 
 def main():
     # Test reading from file
-    path = "data/easy/sat/Analiza1-itox_vc1033.cnf"
-    print(read_dimcas_file(path))
+    path = "data/test-cnf.cnf"
+    file = read_dimcas_file(path)
+
+    test = Statement(file)   
 
     return
 
 main()
-# to ponizej ja dodalem, jak widzisz to ianczej to wywal
-# import statement
-# def analyseFile(filePath):
-#     file=open(filePath)
-#     file.readline()
-#     statement_string = ""
-#     for line in file:
-#         statement_string += line
-#     return statement.Statement(statement_string)
-#
-#
-#
-#
-# test=analyseFile("asset/test1.cnf")
-#
-#
-# for i in test.clauses:
-#     print(i)
-#
-# print(test.variables_already_existing)
-# for i in test.variables:
-#     print(i)
