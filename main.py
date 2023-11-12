d6b030edfc7a892087faf7e724819456d15f8817
GAL_son <pancerek1@gmail.com>
2023-11-12 15:46:42 +0100

    Test Method 1

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from utils import read_dimcas_file
 from statement import Statement
 
+
+from methods.test.test1 import methodTest1
+
 def main():
     # Test reading from file
     path = "data/test-cnf.cnf"
+    path2 = "data/easy/sat/Analiza1-itox_vc1033.cnf"
     file = read_dimcas_file(path)
-
     test = Statement(file)   
+
+    methodTest1(test)
 
     return
 
---
+++ methods/test/test1.py
@@ -0,0 +1,32 @@
+import igraph as ig
+import matplotlib.pyplot as plt
+
+from statement import Statement
+from clause import Clause
+
+def methodTest1(statement):
+    if type(statement) != Statement:
+        raise ValueError("Argument is not Statement type")
+    
+    # In this graph each vertex represents one clause
+    vert_num = len(statement.clauses)
+
+    # Each vertex is connected if there is at least one common variable
+    connections=[]
+    for i in range(0,vert_num):
+        for j in range(i+1, vert_num):
+            clause1 = statement.clauses[i]
+            clause2 = statement.clauses[j]
+            for v in clause1.variables:
+                if v in clause2.variables:
+                    connections.append([i,j])
+                    break
+    
+                    
+    g = ig.Graph(n=vert_num, edges=connections)
+    fig, ax = plt.subplots()
+    ig.plot(g, target=ax)
+
+    plt.show()
+    
+    
