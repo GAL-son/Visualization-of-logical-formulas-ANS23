974729bf580d348c9c4ed42cc809b2ca8a55dd84
GAL_son <pancerek1@gmail.com>
2023-12-29 22:38:53 +0100

    backend fix

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import utils as ut
 from statement import Statement
 
-from methods.test.test1 import methodTest1
-from methods.test.test2 import test2
-from methods.graph_reduction import resolution_graph_reduction
-from methods.resolution_graph import resolution_graph_no_index
+import pipeline as pi
 
 def main():
-    # Test reading from file
-    path = "data/test-cnf.cnf"
-    path2 = "asset/test1.cnf"
-    path3 = "asset/testshort.cnf"
-    path4 = "asset/selfTest.cnf"
-    path5 = "data/medium/unsat/Analiza4-aaai10-planning-ipc5-pathways-17-step20.cnf"
-    path6 = "asset/testChain.cnf"
-    path7 = "asset/graph-red-test.cnf"
-    file = ut.read_dimcas_file(path7)
-    test = Statement(file)   
+    path = "./asset/testChain.cnf"
+    file = ut.read_dimcas_file(path)
+    
+    
+    red_graph = pi.graph_reduction_pipeline(file, 2)
+    res_graph = pi.resolution_graph_pipeline(file)
 
-    # methodTest1(test)
-    # test2(test) 
-    print(ut.graph_to_JSON(resolution_graph_reduction(test)))
-    print(ut.graph_to_JSON(resolution_graph_no_index(test)))
+    print(res_graph)
+
+    ut.argument_to_file_name_specified(res_graph, "RESGRAPH.json")
+    ut.argument_to_file_name_specified(red_graph, "REDGRAPH.json")
 
     return
 
